0ca38742e2ee85088de1b498c084fc6f479caaf2
John McBride <johnrobertmcbride@gmail.com>
2025-07-19 20:03:12 +0100

    update version as deployed old code as a version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.pixelsabre.lockinos"
         minSdk = 28
         targetSdk = 36
-        versionCode = 12
-        versionName = "1.11"
+        versionCode = 13
+        versionName = "1.12"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
